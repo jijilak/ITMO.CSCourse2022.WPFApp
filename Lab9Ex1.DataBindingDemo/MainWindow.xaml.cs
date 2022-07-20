587fc1a20eed071b0dc5d8e0764660a127d88a03
jijilak <95184520+jijilak@users.noreply.github.com>
2022-07-21 02:58:39 +0300

    Lab9Ex1

--- Lab9Ex1.DataBindingDemo/MainWindow.xaml.cs
+++ Lab9Ex1.DataBindingDemo/MainWindow.xaml.cs
@@ -12,12 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 namespace Lab9Ex1.DataBindingDemo
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
---
+++ Lab9Ex1.DataBindingDemo/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9Ex1.DataBindingDemo
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public bool IsEnrolled { get; set; }
+    }
+    public class StudentList : ObservableCollection<Student>
+    {
+
+    }
+}
