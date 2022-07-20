e0b95fcd7121dace0ab38df76b7df315ba13f5c7
jijilak <95184520+jijilak@users.noreply.github.com>
2022-07-20 22:22:02 +0300

    Lab4Ex3

--- Lab4Ex3.UserIn3/MainWindow.xaml.cs
+++ Lab4Ex3.UserIn3/MainWindow.xaml.cs
@@ -23,6 +23,38 @@ namespace Lab4Ex3.UserIn3
         public MainWindow()
         {
             InitializeComponent();
+            foreach (FontFamily F in Fonts.SystemFontFamilies)
+            {
+                listBox1.Items.Add(F.ToString());
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            richTextBox1.Selection.ApplyPropertyValue(FontWeightProperty
+                , FontWeights.Bold);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            richTextBox1.Selection.ApplyPropertyValue(FontStyleProperty
+                , FontStyles.Italic);
+        }
+
+        private void Slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            try
+            {
+                richTextBox1.Selection.ApplyPropertyValue(FontSizeProperty
+                , Slider1.Value.ToString());
+            }
+            catch { }
+        }
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            richTextBox1.Selection.ApplyPropertyValue(FontFamilyProperty
+                , listBox1.SelectedItem);
         }
     }
 }
